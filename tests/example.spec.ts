ef120104227e7f7c8c05be9f010860000b59b692
Yury Semikhatsky <yurys@chromium.org>
2023-09-22 16:52:31 -0700

    argos upload

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import {argosScreenshot} from '@argos-ci/playwright';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -6,6 +7,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
   await expect(page).toHaveScreenshot();
+  await argosScreenshot(page, 'title');
 });
 
 test('get started link', async ({ page }) => {
@@ -17,4 +19,5 @@ test('get started link', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
   await expect(page).toHaveScreenshot();
+  await argosScreenshot(page, 'get started link');
 });
